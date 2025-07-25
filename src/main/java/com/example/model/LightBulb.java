32b50c1ee0b30d184f3e96961b8582419088b7c5
yash031299 <vishwakarmayash24@gmail.com>
2025-07-25 16:07:07 +0530

    Added the basic default validators

--- src/main/java/com/example/model/LightBulb.java
+++ src/main/java/com/example/model/LightBulb.java
@@ -1,11 +1,18 @@
 package com.example.model;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 public class LightBulb {
 
     private Long id;
 
+    @NotBlank(message = "Name ids Required")
     private String name;
+
+    @NotBlank(message = "Type is required")
     private String type;
+
+    @Min(value = 1, message = "Wattage must be at least 1")
     private int wattage;
 
     public Long getId() {
