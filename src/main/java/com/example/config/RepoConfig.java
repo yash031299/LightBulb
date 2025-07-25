fb2b7a46f02c858afe44883c368c9c9951df53f0
yash031299 <vishwakarmayash24@gmail.com>
2025-07-25 16:10:28 +0530

    Using runtime environment for the database selection.

--- src/main/java/com/example/config/RepoConfig.java
+++ src/main/java/com/example/config/RepoConfig.java
@@ -3,18 +3,22 @@ package com.example.config;
 import com.example.repo.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class RepoConfig {
 
     @Bean
-    public LightBulbRepository lightBulbRepository() {
-        String env = System.getenv("AWS_LAMBDA_FUNCTION_NAME");
-        if (env != null) {
-            return new S3LightBulbRepository(S3Client.create());
-        } else {
-            return new LocalLightBulbRepository();
-        }
+    @Profile("local")
+    public LightBulbRepository localRepo() {
+        return new LocalLightBulbRepository();
     }
+
+    @Bean
+    @Profile("s3")
+    public LightBulbRepository s3Repo() {
+        return new S3LightBulbRepository(S3Client.create());
+    }
+
 }
